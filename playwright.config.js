1af51f7d2cf37b3c85e06dcbff1ce4c421eb63cf
PavelProjects <paveloalexeev52@gmail.com>
2023-12-19 23:26:53 +0300

    added ui tests

--- playwright.config.js
+++ playwright.config.js
@@ -13,14 +13,14 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
+  workers: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,27 +28,29 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: "http://192.168.2.2"
+    baseURL: "http://192.168.2.2",
+    // baseURL: "http://localhost:5173",
+    testIdAttribute: 'id',
   },
 
   /* Configure projects for major browsers */
-  // projects: [
+  projects: [
     // {
     //   name: 'chromium',
     //   use: { ...devices['Desktop Chrome'] },
     // },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
     // },
 
-    /* Test against mobile viewports. */
+    // /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
@@ -58,7 +60,7 @@ export default defineConfig({
     //   use: { ...devices['iPhone 12'] },
     // },
 
-    /* Test against branded browsers. */
+    // /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
@@ -67,7 +69,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  // ],
+  ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
---
+++ tests/deviceUi.spec.js
@@ -0,0 +1,111 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeEach('Open control panel', async ({ page }) => {
+  await page.goto("/");
+})
+
+test('Information tab', async ({ page }) => {
+  const respInfo = page.waitForResponse((response) => response.url().endsWith("/info/system"));
+  await page.getByTestId("info").click();
+  await expect(page.getByRole("heading", { name: "Device information" })).toBeVisible();
+
+  await respInfo;
+  await expect(page.locator(".field-label"), "Information fields present")
+    .toHaveText(["Device name", "Device type", "Firmware version", "Chip model", "Chip revision"]);
+
+  const name = "autotest_" + Math.floor(Math.random() * 100);
+  await page.getByTestId("device-name").fill(name);
+
+  const response = page.waitForResponse((response) => response.url().endsWith("/info/system"));
+  await page.getByTestId("save-device-name").click();
+  await response;
+
+  await expect(page.getByTestId("toast-description"), "Name updated").toHaveText("New device name: " + name);
+});
+
+test('Actions and states tab', async ({ page }) => {
+  const statesItem = page.getByTestId('states');
+  const actionsItem = page.getByTestId('actions');
+
+  await statesItem.click();
+  const actionsResp = page.waitForResponse((response) => response.url().endsWith("/actions"));
+  await actionsItem.click();
+  await actionsResp;
+
+  const actionsStates = [
+    {
+      action: "led_on",
+      state: "on"
+    },
+    {
+      action: "led_off",
+      state: "off"
+    },
+  ];
+
+  for (const { action, state } of actionsStates) {
+    await actionsItem.click();
+
+    const actionResp = page.waitForResponse(
+      (response) => response.url().endsWith("/actions?action=" + action)
+    );
+    await page.getByTestId("action_" + action).click();
+    expect((await actionResp).ok()).toBeTruthy();
+
+    await statesItem.click();
+    const statesResp = page.waitForResponse((response) => response.url().endsWith("/states"));
+    await page.getByTestId('menu-main-update').click();
+    await statesResp;
+
+    await expect(page.getByTestId('state-menu-led')).toHaveText("led: " + state);
+  }
+});
+
+test('Sensors tab', async ({ page }) => {
+  const sensors = page.waitForResponse((response) => response.url().endsWith("/sensors"));
+  await page.getByTestId('sensors').click();
+  await sensors;
+
+  await expect(page.getByText("button (digital):")).toBeVisible();
+});
+
+test('Configuration tab', async ({ page }) => {
+  const updateBtn = page.getByTestId('menu-main-update');
+
+  const waitResp = async (action, path="/config") => {
+    const response = page.waitForResponse(
+      (r) => r.url().endsWith(path) && !r.url().endsWith("/info/config")
+    );
+    await action;
+    return await response;
+  }
+
+  await waitResp(page.getByTestId('configuration').click())
+
+  const value = String(Math.floor(Math.random() * 100));
+  const input = page.getByTestId("test_setting");
+  await input.fill(value);
+
+  const res = await waitResp(page.getByTestId("config-save").click());
+  expect(res.ok(), "Config saved").toBeTruthy();
+
+  await waitResp(updateBtn.click());
+  await expect(input, "Config value present").toHaveValue(value);
+
+  page.on('dialog', dialog => dialog.accept());
+  const resDel = await waitResp(page.getByTestId("config-delete").click(), "/config/delete/all");
+  expect(resDel.ok(), "Config deleted").toBeTruthy();
+
+  await waitResp(updateBtn.click());
+  await expect(input, "Config clear").toBeEmpty();
+});
+
+test('Metrics tab', async ({ page }) => {
+  const resp = page.waitForResponse((r) => r.url().endsWith("/metrics"));
+  await page.getByTestId("metrics").click();
+  await resp;
+
+  await expect(page.getByText("uptime:")).toBeVisible();
+  await expect(page.getByText("heap:")).toBeVisible();
+  await expect(page.getByText("counts:")).toBeVisible();
+});
